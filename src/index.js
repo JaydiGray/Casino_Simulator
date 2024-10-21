01b4db5527edc7d3d24f3710410fc8156190d9d3
JaydiGray <evshamatrin@gmail.com>
2024-10-22 02:19:02 +0300

    final_fersion

--- src/index.js
+++ src/index.js
@@ -3,41 +3,53 @@ import Notiflix from 'notiflix';
 const container = document.querySelector('.js-container');
 const start = document.querySelector('.js-start');
 const balance = document.querySelector('.js-balance');
+const rekv = document.querySelector('.js-rekv');
 
 start.addEventListener('click', onStart);
 
 function onStart() {
   if (!Number(balance.textContent)) {
     Notiflix.Notify.failure('Ти на нулі! Час задонатити!');
+    rekv.style.display = 'block';
     return;
   }
 
   balance.textContent = Number(balance.textContent) - 50;
 
-  const result = [];
+  const result = {};
 
   [...container.children].forEach(
-    (item, idx) =>
-      item.classList.remove('banan') || item.classList.remove('seven')
+    item =>
+      item.classList.remove('banan') ||
+      item.classList.remove('seven') ||
+      item.classList.remove('cherry') ||
+      item.classList.remove('apple') ||
+      item.classList.remove('orange')
   );
 
   [...container.children].forEach((item, idx) => {
-    createPromise(idx)
+    createPromise(idx + 1)
       .then(val => {
-        result.push('1');
+        result[val] = (result[val] || 0) + 1;
         item.classList.add(val);
       })
-      .catch(val => {
-        item.classList.add(val);
-      })
+      .catch(() => {})
       .finally(() => {
         setTimeout(() => {
           if (idx === container.children.length - 1) {
-            if (!result.length || result.length === 3) {
+            let counter = 0;
+            for (let item of Object.keys(result)) {
+              counter = counter > result[item] ? counter : result[item];
+            }
+
+            if (counter === 3) {
               Notiflix.Notify.success('Вітаю, це було ПОТУЖНО!');
-              balance.textContent = Number(balance.textContent) + 500;
+              balance.textContent = Number(balance.textContent) + 1000;
+            } else if (counter === 2) {
+              Notiflix.Notify.info('Наступного разу пощастить!');
+              balance.textContent = Number(balance.textContent) + 100;
             } else {
-              Notiflix.Notify.warning('Це була потужня спроба, давай ще!');
+              Notiflix.Notify.warning('Не засмучуйся, крути ще!');
             }
           }
         }, 500);
@@ -48,14 +60,10 @@ function onStart() {
 function createPromise(delay) {
   return new Promise((res, rej) => {
     setTimeout(() => {
-      const random = Math.random();
-      const arr = ['seven'];
-      const first = Math.floor(Math.random() * 10);
-      if (random > 0.5) {
-        res('banan');
-      } else {
-        rej('seven');
-      }
+      const arr = ['seven', 'banan', 'cherry', 'apple', 'orange'];
+      const first = Math.floor(Math.random() * 5);
+
+      res(arr[first]);
     }, delay * 1000);
   });
 }
